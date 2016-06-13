82e1ac2fb7b38b34c308a3c3129f30005458fe55
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-06-13 14:08:06 +0800

    brfss_corr.py

--- brfss_corr.py
+++ brfss_corr.py
@@ -4,6 +4,8 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2010 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+NAME: brfss_corr.py
 """
 
 import math
@@ -14,6 +16,44 @@ import sys
 import brfss_scatter
 import correlation
 import _19_brfss
+
+
+def _log(t):
+    """Computes the log of a sequence."""
+    return [math.log(x) for x in t]
+
+
+def _compute_correlations():
+    resp = brfss_scatter.Respondents()
+    resp._read_records()
+    print('Number of records:', len(resp.records))
+
+    heights, weights = resp.GetHeightWeight()
+    pearson = correlation.Corr(heights, weights)
+    print('Pearson correlation (weights):', pearson)
+
+    log_weights = _log(weights)
+    pearson = correlation.Corr(heights, log_weights)
+    print('Pearson correlation (log weights):', pearson)
+
+    spearman = correlation.SpearmanCorr(heights, weights)
+    print('Spearman correlation (weights):', spearman)
+
+    inter, slope = correlation.LeastSquares(heights, log_weights)
+    print('Least squares inter, slope (log weights):', inter, slope)
+
+    res = correlation.Residuals(heights, log_weights, inter, slope)
+    R2 = correlation.CoefDetermination(log_weights, res)
+    print('Coefficient of determination:', R2)
+    print('sqrt(R^2):', math.sqrt(R2))
+
+
+def main(name):
+    _compute_correlations()
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
 
 """
 Results:
@@ -31,43 +71,4 @@ might be the best choice.
 If we didn't know what transform to use, I would be more inclined to
 use Spearman's correlation, but I am less comfortable with it because
 mapping to ranks is an information-losing transform, and Log is not.
-
 """
-
-
-def Log(t):
-    """Computes the log of a sequence."""
-    return [math.log(x) for x in t]
-
-
-def ComputeCorrelations():
-    resp = brfss_scatter.Respondents()
-    resp._read_records()
-    print('Number of records:', len(resp.records))
-
-    heights, weights = resp.GetHeightWeight()
-    pearson = correlation.Corr(heights, weights)
-    print('Pearson correlation (weights):', pearson)
-
-    log_weights = Log(weights)
-    pearson = correlation.Corr(heights, log_weights)
-    print('Pearson correlation (log weights):', pearson)
-
-    spearman = correlation.SpearmanCorr(heights, weights)
-    print('Spearman correlation (weights):', spearman)
-
-    inter, slope = correlation.LeastSquares(heights, log_weights)
-    print('Least squares inter, slope (log weights):', inter, slope)
-
-    res = correlation.Residuals(heights, log_weights, inter, slope)
-    R2 = correlation.CoefDetermination(log_weights, res)
-    print('Coefficient of determination:', R2)
-    print('sqrt(R^2):', math.sqrt(R2))
-
-
-def main(name):
-    ComputeCorrelations()
-
-
-if __name__ == '__main__':
-    main(*sys.argv)
