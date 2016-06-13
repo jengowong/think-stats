faad3f94291a2337bb694bddfd8e41f0ed518fd0
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-06-13 10:51:15 +0800

    _12_score_example.py

--- _12_score_example.py
+++ _12_score_example.py
@@ -4,10 +4,12 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2010 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+NAME: _12_score_example.py
 """
 
 
-def PercentileRank(scores, your_score):
+def _percentile_rank(scores, your_score):
     """Computes the percentile rank relative to a sample of scores."""
     count = 0
     for score in scores:
@@ -23,19 +25,19 @@ your_score = 88
 
 print('score, percentile rank')
 for score in scores:
-    print(score, PercentileRank(scores, score))
+    print(score, _percentile_rank(scores, score))
 print
 
 
-def Percentile(scores, percentile_rank):
+def _percentile(scores, percentile_rank):
     """Computes the value that corresponds to a given percentile rank. """
     scores.sort()
     for score in scores:
-        if PercentileRank(scores, score) >= percentile_rank:
+        if _percentile_rank(scores, score) >= percentile_rank:
             return score
 
 
-def Percentile2(scores, percentile_rank):
+def _percentile2(scores, percentile_rank):
     """
     Computes the value that corresponds to a given percentile rank.
 
@@ -48,6 +50,6 @@ def Percentile2(scores, percentile_rank):
 
 print('prank, score, score')
 for percentile_rank in [0, 20, 25, 40, 50, 60, 75, 80, 100]:
-    print(percentile_rank, )
-    print(Percentile(scores, percentile_rank), )
-    print(Percentile2(scores, percentile_rank))
+    print(percentile_rank,)
+    print(_percentile(scores, percentile_rank),)
+    print(_percentile2(scores, percentile_rank))
