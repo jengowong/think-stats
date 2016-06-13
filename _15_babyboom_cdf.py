ae9fb3f653ba4825365fc35fa93870ad71f6f574
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-06-13 11:04:47 +0800

    _15_babyboom_cdf.py

--- _15_babyboom_cdf.py
+++ _15_babyboom_cdf.py
@@ -1,3 +1,7 @@
+"""
+NAME: _15_babyboom_cdf.py
+"""
+
 import os
 import sys
 import _04_Pmf
@@ -5,7 +9,7 @@ import _05_myplot
 import _13_Cdf
 
 
-def ReadTimeLength():
+def _read_time_length():
     filename = os.path.join('.', 'babyboom.csv')
     fp = open(filename)
     result = []
@@ -18,16 +22,16 @@ def ReadTimeLength():
     return result
 
 
-def CalcTimeInterval():
-    timeLength = ReadTimeLength()
+def _calc_time_interval():
+    timeLength = _read_time_length()
     print('timeLength\'s length=', len(timeLength))
     print('range(len(timeLength))-->', range(len(timeLength)))
     timeInterval = [timeLength[i + 1] - timeLength[i] for i in range(len(timeLength) - 1)]
     return timeInterval
 
 
-def CdfTimeInterval():
-    timeInterval = CalcTimeInterval()
+def _cdf_time_interval():
+    timeInterval = _calc_time_interval()
     pmf = _04_Pmf._make_pmf_from_list(timeInterval, 'baby birth interval')
     _05_myplot._clf()
     _05_myplot._hist(pmf)
@@ -46,8 +50,8 @@ def CdfTimeInterval():
                      ylabel='cumulative probability')
 
 
-def LogCCdfTimeIntervel():
-    timeInterval = CalcTimeInterval()
+def _log_cdf_time_interval():
+    timeInterval = _calc_time_interval()
     pmf = _04_Pmf._make_pmf_from_list(timeInterval, 'baby birth interval')
     cdf = _13_Cdf._make_cdf_from_pmf(pmf, 'baby birth interval')
     _05_myplot._clf()
@@ -62,4 +66,4 @@ def LogCCdfTimeIntervel():
 # print ReadTimeLength()
 # CdfTimeInterval()
 # print sorted(CalcTimeInterval())
-LogCCdfTimeIntervel()
+_log_cdf_time_interval()
