8d7ccd62c5fcfccb4405c6262b488ea601b989f6
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-06-14 10:40:29 +0800

    pareto_world.py

--- pareto_world.py
+++ pareto_world.py
@@ -4,6 +4,8 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2011 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+NAME: pareto_world.py
 """
 
 import matplotlib.pyplot as pyplot
@@ -12,19 +14,19 @@ import _05_myplot
 import _13_Cdf
 
 
-def ParetoCdf(x, alpha, xmin):
+def _pareto_cdf(x, alpha, xmin):
     """Evaluates CDF of the Pareto distribution with parameters alpha, xmin."""
     if x < xmin:
         return 0
     return 1 - pow(x / xmin, -alpha)
 
 
-def ParetoMedian(xmin, alpha):
+def _pareto_median(xmin, alpha):
     """Computes the median of a Pareto distribution."""
     return xmin * pow(2, 1 / alpha)
 
 
-def MakeParetoCdf():
+def _make_pareto_cdf():
     """Generates a plot of the CDF of height in Pareto World."""
     n = 50
     max = 1000.0
@@ -32,8 +34,8 @@ def MakeParetoCdf():
 
     xmin = 100
     alpha = 1.7
-    ps = [ParetoCdf(x, alpha, xmin) for x in xs]
-    print('Median', ParetoMedian(xmin, alpha))
+    ps = [_pareto_cdf(x, alpha, xmin) for x in xs]
+    print('Median', _pareto_median(xmin, alpha))
 
     pyplot.clf()
     pyplot.plot(xs, ps, linewidth=2)
@@ -44,16 +46,17 @@ def MakeParetoCdf():
                      legend=False)
 
 
-def MakeFigure(xmin=100, alpha=1.7, mu=150, sigma=25):
+def _make_figure(xmin=100, alpha=1.7, mu=150, sigma=25):
     """
     Makes a figure showing the CDF of height in ParetoWorld.
 
     Compared to a normal distribution.
 
-    xmin:  parameter of the Pareto distribution
-    alpha: parameter of the Pareto distribution
-    mu:    parameter of the Normal distribution
-    sigma: parameter of the Normal distribution
+    Args:
+        xmin:  parameter of the Pareto distribution
+        alpha: parameter of the Pareto distribution
+        mu:    parameter of the Normal distribution
+        sigma: parameter of the Normal distribution
     """
     t1 = [xmin * random.paretovariate(alpha) for i in range(10000)]
     cdf1 = _13_Cdf._make_cdf_from_list(t1, name='pareto')
@@ -69,14 +72,15 @@ def MakeFigure(xmin=100, alpha=1.7, mu=150, sigma=25):
                      ylabel='CDF')
 
 
-def TallestPareto(iters=2, n=10000, xmin=100, alpha=1.7):
+def _tallest_pareto(iters=2, n=10000, xmin=100, alpha=1.7):
     """
     Find the tallest person in Pareto World.
 
-    iters: how many samples to generate
-    n:     how many in each sample
-    xmin:  parameter of the Pareto distribution
-    alpha: parameter of the Pareto distribution
+    Args:
+        iters: how many samples to generate
+        n:     how many in each sample
+        xmin:  parameter of the Pareto distribution
+        alpha: parameter of the Pareto distribution
     """
     tallest = 0
     for i in range(iters):
@@ -86,9 +90,9 @@ def TallestPareto(iters=2, n=10000, xmin=100, alpha=1.7):
 
 
 def main():
-    MakeFigure()
-    MakeParetoCdf()
-    print(TallestPareto(iters=2))
+    _make_figure()
+    _make_pareto_cdf()
+    print(_tallest_pareto(iters=2))
 
 
 if __name__ == "__main__":
